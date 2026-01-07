a0da5bc998ef19c221190963963efe6bf2268fad
DevDoshi19 <devdoshi1927@gmail.com>
2026-01-07 22:42:54 +0530

    updated code

---
+++ day-7/Build_Array_from_Permutation.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = [nums[nums[i]] for i in range(len(nums))]
+
+        # for i in range(len(nums)):
+        #     ans.append(nums[nums[i]])
+
+        return ans    
+    
+# Example usage:
+nums = [0,2,1,5,3,4]
+solution = Solution().buildArray(nums)
+print(solution)  # Output: [0, 1, 2, 4, 5, 3]
