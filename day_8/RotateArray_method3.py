d2ac2a90130642dde85871e2abb658f5bbae0339
DevDoshi19 <devdoshi1927@gmail.com>
2026-01-09 23:50:48 +0530

    updated code

--- day_8/RotateArray_method3.py
+++ day_8/RotateArray_method3.py
@@ -6,8 +6,16 @@ class Solution:
         """
         n = len(nums)
         k = k % n
-        # solution - 1 
-        nums[:] = nums[n-k:] + nums[:n-k]
+        # solution - 3
+        def reverse(nums,left,right):
+            while left < right :
+                nums[left],nums[right] = nums[right],nums[left]
+                left += 1
+                right -= 1
+            
+        reverse(nums,n-k,n-1)
+        reverse(nums,0,n-k-1)
+        reverse(nums,0,n-1)
 
 # Example usage:
 nums = [1,2,3,4,5,6,7]
