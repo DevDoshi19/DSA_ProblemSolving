ab0057de8f79a620ca6da9015df34f6d07f37e5a
DevDoshi19 <devdoshi1927@gmail.com>
2026-01-15 23:12:54 +0530

    day-13

---
+++ day_13/longest-consecutive-sequence_128_m1.py
@@ -0,0 +1,27 @@
+from typing import List
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        
+        if len(nums) == 0:
+            return 0 
+
+        nums.sort()
+        count,max_count = 1,1
+
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1] :
+                continue
+            elif nums[i] == nums[i-1] + 1 :
+                count +=1
+
+            else:
+                count = 1
+
+            max_count = max(count,max_count)
+
+        return max_count
+    
+# Example 
+solution = Solution()
+nums=[100,4,200,1,3,2,101,6,7,104,102,103]
+print(solution.longestConsecutive(nums=nums))
