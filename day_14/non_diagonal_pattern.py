a9b6d0ca84312f0223f39833b622d953c46612a3
DevDoshi19 <devdoshi1927@gmail.com>
2026-01-16 23:14:55 +0530

    day-14

--- day_14/non_diagonal_pattern.py
+++ day_14/non_diagonal_pattern.py
@@ -1,3 +1,6 @@
+# Print the non-diagonal pattern from the given 2D array
+# Example Input:
+
 nums = [[5,10,8],[7,6,3],[2,1,9]]
 
 row = len(nums)
@@ -10,3 +13,21 @@ for i in range(row):
         else:
             print(" ", end=" ")
     print()
+
+'''
+what we have done here is we have iterated through each element of the 2D array
+and printed the element only if it is not on the diagonal (i.e., when column index j is not equal to row index i)
+
+- otherwise we print a space to maintain the structure of the array
+
+- The time complexity of this approach is O(n^2) where n is the number of rows (or columns) in the 2D array
+- The space complexity is O(1) as we are not using any extra space that grows
+
+with the input size.
+~ Example Output:
+  
+        8
+     6    
+  2
+
+'''
