e0e567b8f3fb48e9f348ca24b063a9d1a0e2556b
DevDoshi19 <devdoshi1927@gmail.com>
2026-01-25 23:50:54 +0530

    day_19

--- day_19/search-insert-position_53_optimal.py
+++ day_19/search-insert-position_53_optimal.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         n = len(nums)
@@ -13,3 +14,8 @@ class Solution:
                 low = mid+1
 
         return lb 
+    
+solution = Solution()
+print(solution.searchInsert([1,3,5,6],5))  # Output: 2
+print(solution.searchInsert([1,3,5,6],2))  # Output: 1
+print(solution.searchInsert([1,3,5,6],7))  # Output: 4
