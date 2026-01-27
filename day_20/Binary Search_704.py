1e95afa56d725fa983f68b0ea84cadaf056d363f
DevDoshi19 <devdoshi1927@gmail.com>
2026-01-27 23:41:34 +0530

    day_20

--- day_20/Binary Search_704.py
+++ day_20/Binary Search_704.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
@@ -13,3 +14,8 @@ class Solution:
                 left = mid + 1   # Search right
         
         return -1
+    
+# Example usage:
+sol = Solution()
+print(sol.search([-1,0,3,5,9,12], 9))  # Output: 4
+print(sol.search([-1,0,3,5,9,12], 2))  # Output: -1
