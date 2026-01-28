2aed793ce1fdd00262716569ed27c0cff5827105
DevDoshi19 <devdoshi1927@gmail.com>
2026-01-28 22:14:46 +0530

    day_21

--- day_21/sqrtx_69_easy.py
+++ day_21/sqrtx_69_easy.py
@@ -2,3 +2,8 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         sq = int(x**0.5)
         return sq
+    
+# Example usage:
+sol = Solution()
+print(sol.mySqrt(4))   # Output: 2
+print(sol.mySqrt(8))   # Output: 2
