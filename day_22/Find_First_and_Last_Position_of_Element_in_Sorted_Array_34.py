b97316c98a0a6caa21c315e4b557f5bfda7eae32
DevDoshi19 <devdoshi1927@gmail.com>
2026-01-29 23:52:38 +0530

    day_22

---
+++ day_22/Find_First_and_Last_Position_of_Element_in_Sorted_Array_34.py
@@ -0,0 +1,33 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1, -1]
+        
+        n = len(nums)
+        
+        # Find lower bound (first occurrence)
+        left, right = 0, n - 1
+        lb = n
+        
+        while left <= right :
+            mid = (left+right)//2
+            if nums[mid] >= target:
+                lb = mid
+                right = mid - 1 
+            else :
+                left = mid+1
+        
+        left,right = 0,n-1
+        ub = n 
+        while left <= right :
+            mid = (left+right)//2
+            if nums[mid] > target:
+                ub = mid
+                right = mid - 1 
+            else :
+                left = mid+1
+
+        if lb < n and ub > 0 and lb <= ub-1 : 
+            return [lb,ub-1]
+
+        return [-1,-1]       
