aae88f80ac088ef98599ba273f2ff9f8df416ab5
DevDoshi19 <devdoshi1927@gmail.com>
2026-01-29 23:53:47 +0530

    day_22

--- day_22/Find_First_and_Last_Position_of_Element_in_Sorted_Array_34.py
+++ day_22/Find_First_and_Last_Position_of_Element_in_Sorted_Array_34.py
@@ -32,3 +32,11 @@ class Solution:
             return [lb,ub-1]
 
         return [-1,-1]       
+    
+# Example usage:
+sol = Solution()
+print(sol.searchRange([5,7,7,8,8,10], 8))  # Output: [3, 4]
+print(sol.searchRange([5,7,7,8,8,10], 6))  # Output: [-1, -1]
+# LeetCode Problem 34: Find First and Last Position of Element in Sorted Array - Medium
+# Given an array of integers nums sorted in ascending order, find the starting and ending position of
+# a given target value. If the target is not found in the array, return [-1, -1].
